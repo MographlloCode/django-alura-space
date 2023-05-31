fa07b20b2042756ea8e39b3c01266fcef5ad2320
Gustavo Mello <mographllo@gmail.com>
2023-05-31 13:44:55 -0300

    Created Picture model

--- galery/models.py
+++ galery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Picture(models.Model):
+    ''' Model for Picture '''
+
+    name = models.CharField(max_length=60, null=False, blank=False)
+    subtitle = models.CharField(max_length=120, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    picture = models.CharField(max_length=60, null=False, blank=False)
+
+    def __str__(self):
+        return f'Picture [name={self.name}]'
--- galery/views.py
+++ galery/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'galery/index.html')
+
+    data = {
+        1: {"name": "Alpha's Nebula", "description": "GalaxyExplore.com / Alpha Vega / StarFinder"},
+        2: {"name": "Luna's Aurora", "description": "SpaceVision.net / Luna Anderson / MoonScope"},
+        3: {"name": "Nova's Cosmos", "description": "StellarAdventures.com / Nova Patel / AstroQuest"},
+        4: {"name": "Orion's Galaxy", "description": "AstroExplorers.org / Orion Thompson / Cosmoscope"},
+        5: {"name": "Celeste's Stardust", "description": "SkyWonders.com / Celeste Ramirez / StarGaze"},
+        6: {"name": "Vega's Cosmos", "description": "StarSearch.net / Vega Li / AstroLens"},
+        7: {"name": "Aurora's Nebula", "description": "GalacticVisions.com / Aurora Kim / NebulaScope"},
+        8: {"name": "Sirius's Stardust", "description": "CelestialAdventures.org / Sirius Chen / SkyGlimpse"},
+        9: {"name": "Stella's Cosmos", "description": "AstroJourneys.com / Stella Johnson / StarGazer"},
+        10: {"name": "Solaris's Aurora", "description": "CosmicExplorations.net / Solaris Garcia / AuroraLens"},
+    }
+
+    return render(request, 'galery/index.html', {"cards": data})
 
 def image(request):
     return render(request, 'galery/image.html')
